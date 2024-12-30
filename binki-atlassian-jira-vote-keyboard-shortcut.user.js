f7e04ad4aad57ddd751c305433652a6641ef32b5
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2024-12-29 22:51:54 -0500

    Fix triggering vote toggle when pasting or typing search terms.
    When pasting using ctrl+v, it is common for ctrl to be released prior to releasing v. This resulted in false triggers due to uncancelled keyup events in the contenteditable rich textbox controls. This is fixed by using the keydown event instead of the keyup event. This way, the absence of modifier keys can be correctly detected.
    
    When typing into the search textbox, the letter v was triggering a vote incorrectly. This is due to the keyup event for simpler inputs (non-contenteditable ones) not being marked cancelled by Jira. This is fixed by allowing the default actions of the keydown event to run and detecting the lack of a beforeinput event prior to proceeding as this event will fire in contexts where the input is treated as an input character both for normal inputs and contenteditable. Another facet of this fix is to specifically test for the absence of a select ancestor on the event target as select elements don’t receive beforeinput events.
    
    Fixes #1.

--- binki-atlassian-jira-vote-keyboard-shortcut.user.js
+++ binki-atlassian-jira-vote-keyboard-shortcut.user.js
@@ -6,23 +6,48 @@
 // @require https://github.com/binki/binki-userscript-when-element-query-selector-async/raw/0a9c204bdc304a9e82f1c31d090fdfdf7b554930/binki-userscript-when-element-query-selector-async.js
 // ==/UserScript==
 
-document.body.addEventListener('keyup', e => {
+let triggering = false;
+
+document.body.addEventListener('beforeinput', e => {
+  // We received an event indicating that the character would be used for input, so cancel any speculative handling.
+  triggering = false;
+});
+
+document.body.addEventListener('keydown', e => {
   // Ignore already-handled.
   if (e.defaultPrevented) return;
-  
+
   // Ignore composition
 	if (e.isComposing || e.keyCode === 229) return;
-  
+
+  // Ignore repeats because it is confusing if the vote flips back and forth while the key is held and may actually cause problems with consuming Jira’s API.
+  if (e.repeat) return;
+
   // Only match v (and as a result require shift+v when capslock is on I guess).
   if (e.altKey || e.ctrlKey || e.shiftKey || e.metaKey || e.key !== 'v') return;
 
-  const issueVoteOptionsButtonElement = document.querySelector('[data-testid="issue-field-voters.ui.button.styled-button"]');
-  // Only if we are in an issue view screen.
-  if (!issueVoteOptionsButtonElement) return;
-  (async () => {
-    issueVoteOptionsButtonElement.click();
-  
-    // Click the vote button to expand the dialog.
-    (await whenElementQuerySelectorAsync(document, '[data-testid="issue-field-voters.ui.vote-toggle.tooltip--container"] button')).click();
-  })();
+  // Ignore the key when the target is a select. Specifically test for select so
+  // that we don’t interfere with the user typing to choose a different select option
+  // (since the beforeinput guard doesn’t apply to select elements).
+  if (e.target.closest('input, select')) return;
+
+  // Since it is hard to test for whether or not the default action will actually
+  // cause input, wait for that default action to occur and see if we get a beforeinput
+  // for it.
+  triggering = true;
+  Promise.resolve().then(() => {
+    // Skip because a beforeinput event was fired, meaning that the key was used as input to a textbox or contenteditable.
+    if (!triggering) return;
+
+    const issueVoteOptionsButtonElement = document.querySelector('[data-testid="issue-field-voters.ui.button.styled-button"]');
+    // Only if we are in an issue view screen.
+    if (!issueVoteOptionsButtonElement) return;
+    (async () => {
+      // Click the vote button to expand the dialog.
+      issueVoteOptionsButtonElement.click();
+
+      // Click the vote button in the expanded dialog.
+      (await whenElementQuerySelectorAsync(document, '[data-testid="issue-field-voters.ui.vote-toggle.tooltip--container"] button')).click();
+    })();
+  });
 });
