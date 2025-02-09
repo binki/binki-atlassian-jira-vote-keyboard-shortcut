fec19c7055903f2cbc1682b274db84474a7a4c2f
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2025-02-09 00:32:25 -0500

    binki-atlassian-jira-vote-keyboard-shortcut-1.2.0

--- binki-atlassian-jira-vote-keyboard-shortcut.user.js
+++ binki-atlassian-jira-vote-keyboard-shortcut.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name binki-atlassian-jira-vote-keyboard-shortcut
 // @homepageURL https://github.com/binki/binki-atlassian-jira-vote-keyboard-shortcut
-// @version 1.1.1
+// @version 1.2.0
 // @match https://*.atlassian.net/*
 // @require https://github.com/binki/binki-userscript-when-element-query-selector-async/raw/0a9c204bdc304a9e82f1c31d090fdfdf7b554930/binki-userscript-when-element-query-selector-async.js
 // ==/UserScript==
